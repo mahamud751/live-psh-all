e263275a54205403508c7869d297b8de708a51af
Mahamud Pino <mahamudpino24678@gmail.com>
2023-12-06 16:42:38 +0600

    footer add

--- psh-client/src/components/shared/Footer.jsx
+++ psh-client/src/components/shared/Footer.jsx
@@ -270,7 +270,7 @@ const Footer = () => {
               </ul>
             </div>
           </footer>
-          <div className=" sm:flex md:hidden">
+          <div className=" sm:flex sm:justify-center md:hidden">
               <Link
                 to={"/terms"}
                 className="text-white text-[14px]"
@@ -291,7 +291,7 @@ const Footer = () => {
               </Link>
             </div>
           <div
-            className="text-center flex items-center sm:justify-between md:justify-center text-white py-3 sm:text-[12px]"
+            className="text-center flex items-center justify-center text-white py-3 sm:text-[12px]"
             style={{
               background: "linear-gradient(to right, #020304, #071e37)",
             }}
@@ -317,7 +317,7 @@ const Footer = () => {
                 Privacy & Policy
               </Link>
             </div>
-            <div className="flex md:ms-4 footer_social ms-2 mr-2">
+            <div className="sm:hidden md:flex md:ms-4 footer_social ms-2 mr-2">
               <img src="assets/img/facebook.svg.png" alt="" />
 
               <img src="assets/img/Link → instagram.svg.png" alt="" className="mx-2"/>
